b53b6c01a862263fea142914235be2afff7283fb
den <den@platfarm.net>
2023-02-12 20:12:10 +0900

    fix : pwa fetch

--- pwaPlugin/generageSw.ts
+++ pwaPlugin/generageSw.ts
@@ -19,5 +19,18 @@ export const generateSw = ({
           })()
         );
       });
+      self.addEventListener('fetch', function(event) {
+        event.respondWith(async function() {
+           try{
+             var res = await fetch(event.request);
+             var cache = await caches.open("${cacheName}");
+             cache.put(event.request.url, res.clone());
+             return res;
+           }
+           catch(error){
+             return caches.match(event.request);
+            }
+          }());
+      });
     `;
 };
